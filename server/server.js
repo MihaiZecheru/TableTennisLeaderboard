07310e8332223fca84edce34c8bb9ab264ce64f6
Mihai Zecheru <zecheruchris@gmail.com>
2025-05-12 23:30:11 -0700

    Modify example payload

--- server/server.js
+++ server/server.js
@@ -17,8 +17,8 @@ app.get('/', async (req, res) => {
 app.post('/api/upload-set', async (req, res) => {
   /**
    * Example payload (note player IDs start at 10):
-   * { p1_id: 10, p2_id: 12, set_length: 21, started_at: "1970-01-01 00:00:42", point_history:
-   * [{ 's': 1, 'w': 0 }, { 's': 1, 'w': 1 }] }
+   * { p1_id: 10, p2_id: 12, set_length: 21, started_at: "1970-01-01 00:00:42", ended_at: "1970-01-01 00:10:05",
+   * point_history: [{ 's': 1, 'w': 0 }, { 's': 1, 'w': 1 }] }
    */
   const { p1_id, p2_id, set_length, point_history, started_at, ended_at } = req.body;
   const success = await AddSetToDatabase(p1_id, p2_id, set_length, point_history, started_at, ended_at);
