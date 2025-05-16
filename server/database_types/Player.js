c44b23cd13c522432411e67ad0f22217fb744543
Mihai Zecheru <zecheruchris@gmail.com>
2025-05-15 19:58:55 -0700

    Add total_time field

--- server/database_types/Player.js
+++ server/database_types/Player.js
@@ -1,18 +1,26 @@
 class Player {
   /**
-   * @param {number} id
-   * @param {string} name
-   * @param {number} total_games_played 
-   * @param {number} total_wins 
-   * @param {number} total_losses 
-   * @param {number} points_won 
-   * @param {number} points_lost 
-   * @param {number} longest_win_streak 
-   * @param {number} longest_loss_streak 
-   * @param {Date} created_at 
+   * @param {number} id - Unique identifier for the player
+   * @param {string} name - Name of the player
+   * @param {number} total_games_played - Total number of games played by the player
+   * @param {number} total_wins - Total number of wins by the player
+   * @param {number} total_losses - Total number of losses by the player
+   * @param {number} points_won - Total points won by the player
+   * @param {number} points_lost - Total points lost by the player
+   * @param {number} longest_win_streak - Longest winning streak of the player
+   * @param {number} longest_loss_streak - Longest losing streak of the player
+   * @param {number} longest_game_duration - Longest game duration of the player
+   * @param {number} most_points_in_one_game - Most points scored in a single game by the player
+   * @param {number} biggest_win_margin - Biggest win margin of the player
+   * @param {number} biggest_loss_margin - Biggest loss margin of the player
+   * @param {number} most_points_won_in_a_row - Most points won in a row by the player
+   * @param {number} most_points_lost_in_a_row - Most points lost in a row by the player
+   * @param {number} time_played - Total time played by the player
+   * @param {string} created_at - Date when the player was created in the database
+   * @description Represents a player in the database with various statistics and calculated fields.
    */
   constructor(id, name, total_games_played, total_wins, total_losses, points_won, points_lost, longest_win_streak, longest_loss_streak,
-              longest_game_duration, most_points_in_one_game, biggest_win_margin, biggest_loss_margin, most_points_won_in_a_row, most_points_lost_in_a_row, created_at) {
+              longest_game_duration, most_points_in_one_game, biggest_win_margin, biggest_loss_margin, most_points_won_in_a_row, most_points_lost_in_a_row, time_played, created_at) {
     // Database
     this.id = id; // Unique identifier for the player
     this.name = name; // Name of the player
@@ -33,6 +41,7 @@ class Player {
     this.biggest_loss_margin = biggest_loss_margin; // Biggest loss margin of the player
     this.most_points_won_in_a_row = most_points_won_in_a_row; // Most points won in a row by the player
     this.most_points_lost_in_a_row = most_points_lost_in_a_row; // Most points lost in a row by the player
+    this.time_played = time_played;
     
     // Calculated stats
     this.game_win_ratio = total_wins / (total_games_played || 1); // Player's win ratio
